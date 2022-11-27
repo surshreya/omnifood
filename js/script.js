8080f9f40c04d50191f101af8de17b973713af68
surshreya <surshreya.0727@gmail.com>
2022-11-27 18:35:15 +0530

    smooth scrolling using js added

--- js/script.js
+++ js/script.js
@@ -9,8 +9,38 @@ btnNavEl.addEventListener("click", (e) => {
   headerEl.classList.toggle("nav-open");
 });
 
-/**
+/*
  * Set Copyright year
  */
 const yearEl = document.querySelector(".year");
 yearEl.textContent = new Date().getFullYear();
+
+/*
+ * Implement smooth scrolling
+ */
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    console.log(link.classList);
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
